fbf6da99033245846825d825a0b06c99ee5333a6
Shruthi-S1302 <89682079+Shruthi-S1302@users.noreply.github.com>
2021-11-08 17:22:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -515,4 +515,37 @@
     <script defer src="assets/js/appb222.js?v=214a6e5c0e"></script>
     <script defer src="assets/js/homeb222.js?v=214a6e5c0e"></script>
   
+  </body>
+</html>
+<script>
+$(document).ready(function(){
+
+ load_data();
+
+ function load_data(query)
+ {
+  $.ajax({
+   url:"fetch.php",
+   method:"POST",
+   data:{query:query},
+   success:function(data)
+   {
+    $('#result').html(data);
+   }
+  });
+ }
+ $('#search_text').keyup(function(){
+  var search = $(this).val();
+  if(search != '')
+  {
+   load_data(search);
+  }
+  else
+  {
+   load_data();
+  }
+ });
+});
+</script>
+  
     
