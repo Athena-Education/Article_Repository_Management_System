6d009df142e37d4da46fef3d215aa1b5ee7d3b65
Shruthi-S1302 <89682079+Shruthi-S1302@users.noreply.github.com>
2021-11-08 20:20:43 +0530

    Update login.php

--- login.php
+++ login.php
@@ -58,3 +58,163 @@
         }
       };
     </script>
+    
+    <title>Blog | Athena</title>
+  </head>
+    <style>
+    @font-face {
+        font-family: Orion;
+        src: url(../font/Orion.otf);
+    }
+    </style>
+  <body oncontextmenu="return false;">
+
+    <div class="container">
+      <div class="forms-container">
+        <div class="signin-signup">
+          <form
+            action="welcome.php"
+            class="sign-in-form"
+            id="loginform"
+            method="POST"
+          >
+            <img src="img/logo.png" height="150" width="150" style="border-radius: 100%;" />
+            <a style="text-decoration: none; font-weight: bolder; margin: 10px;" href="https://arms-cse3001.42web.io/">Continue to Blog</a>
+            <h2 class="title" style="font-family: Orion; font-size: 1.5rem; word-spacing: -10px;">Are you Ready?</h2>
+            <div class="input-field">
+              <i class="fas fa-user"></i>
+
+              <input
+                type="text"
+                placeholder="Username"
+                name="uname"
+                autocomplete="off"
+                autocapitalize="off"
+                id="username"
+              />
+            </div>
+            <div class="input-field">
+              <i class="fas fa-lock"></i>
+              <input
+                type="password"
+                placeholder="Password"
+                name="pass"
+                autocomplete="off"
+                id="password"
+              />
+            </div>
+            <small>
+              Please Login using details sent on the registered mails
+            </small>
+            <br />
+            <input
+              type="submit"
+              value="Login"
+              name="login"
+              class="bxtn solid"
+              id="submit"
+            />
+          </form>
+          <?php 
+        require_once  ('connection.php');
+        ?>
+          <form class="sign-up-form" action="signup.php" method="POST">
+            <h2 class="title" style="font-family: Orion; font-size: 1.8rem; word-spacing: -10px;">Sign up</h2>
+            <div class="input-field">
+              <i class="fas fa-feather"></i>
+              <input
+                type="text"
+                placeholder="First Name"
+                autocomplete="off"
+                autocapitalize="off"
+                name="name1"
+                required
+              />
+            </div>
+            <div class="input-field">
+              <i class="fas fa-user-circle"></i>
+              <input
+                type="text"
+                placeholder="Full Name"
+                autocomplete="off"
+                autocapitalize="off"
+                name="name2"
+                required
+              />
+            </div>
+            <div class="input-field">
+              <i class="far fa-envelope"></i>
+              <input
+                type="email"
+                placeholder="Email"
+                autocomplete="off"
+                autocapitalize="off"
+                name="email1"
+                required
+              />
+            </div>
+                        <div class="input-field">
+              <i class="fas fa-user-check"></i>
+              <select id="gender" name="gender">
+                <option value ="choice">Gender: </option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="others">Others</option>
+                </select>
+            </div>
+            <div class="input-field">
+              <i class="fas fa-phone"></i>
+              <input
+                type="tel"
+                placeholder="10-Digit Phone Number"
+                autocomplete="off"
+                autocapitalize="off"
+                name="phone1"
+                required
+              />
+            
+            </div>
+
+            <script
+              src="https://www.google.com/recaptcha/api.js"
+              async
+              defer
+            ></script>
+            <div
+              class="g-recaptcha"
+              data-sitekey="<?=CONTACTFORM_RECAPTCHA_SITE_KEY?>"
+            ></div>
+            <input type="submit" class="bxtn" value="Sign up" name="submit1" />
+          </form>
+        </div>
+      </div>
+
+      <div class="panels-container">
+        <div class="panel left-panel">
+          <div class="content">
+            <h3>Contribute to Athena?</h3>
+            <p>
+              If this is your first time contributing to our community, Kindly
+              proceed to Sign Up
+            </p>
+            <button class="bxtn transparent" id="sign-up-btn">Sign up</button>
+          </div>
+          <img style="height: 350px;" src="img/log.png" class="image" alt="" />
+        </div>
+        <div class="panel right-panel">
+          <div class="content">
+            <h3>Already Registered with Us ?</h3>
+            <p>
+              Those who have given their valuable write-ups and blogs before, Need
+              not to signup again ! Sign In with login details sent on your mail
+            </p>
+            <button class="bxtn transparent" id="sign-in-btn">Sign in</button>
+          </div>
+          <img width="300" height="300" src="img/register.png" class="image" alt="" />
+        </div>
+      </div>
+    </div>
+
+    <script src="js/app.js"></script>
+  </body>
+</html>
